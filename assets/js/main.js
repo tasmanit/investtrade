46e26e78ceed4b374c3b5c84a7a642ec9b461b19
tasmanit <63876838+tasmanit@users.noreply.github.com>
2022-11-22 03:29:53 +0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,19 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
     responsive:{
         0:{
-            items:1
+            items:2
         },
         600:{
             items:3
         },
         1000:{
-            items:5
+            items:4
         }
     }
 })
